e554282c0b236e22c0ef8c6ef8f1fee0f9b19bd1
Andy Kretschmar <akretschmar@infrapros.com>
2020-05-22 12:39:07 -0500

    add documentation; improve error messages; print overall result

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # kustomize-sandbox
 
 A playground for testing various kustomize and argo versions, etc.
+
+## Running the tests
+
+To run the tests for `job-generate-name`, you must have two versions of kustomize available: 1.0.11 and 3.5.  You can either name them `kustomize1` and `kustomize3`, respectively, and ensure that each is in your `$PATH`, or you can set the environment variables `KUSTOMIZE1` and `KUSTOMIZE3` to fully-qualified paths to the binaries, e.g.:
+
+```
+# assumes `kustomize1` and `kustomize3` are in $PATH
+./job-generate-name/tests/tests.sh
+
+# explicitly sets kustomize v1 and v3 locations
+KUSTOMIZE1=/path/to/v1/kustomize KUSTOMIZE3=/path/to/v3/kustomize ./job-generate-name/tests/tests.sh
+```
--- job-generate-name/tests/tests.sh
+++ job-generate-name/tests/tests.sh
@@ -32,23 +32,25 @@ k3=${KUSTOMIZE3:-kustomize3}
 echo "checking for kustomize1..."
 $k1 version | grep KustomizeVersion:1.0.11
 if [ $? -ne 0 ]; then
-    >&2 echo "kustomize1 not found or not correct version"
+    >&2 echo "kustomize v1 not found or not correct version"
     ok=0
 fi
 
 echo "checking for kustomize3..."
 $k3 version | grep Version:3.5
 if [ $? -ne 0 ]; then
-    >&2 echo "kustomize3 not found or not correct version"
+    >&2 echo "kustomize v3 not found or not correct version"
     ok=0
 fi
 
 if [ $ok -ne 1 ]; then
-    >&2 echo "failed to find all dependencies.  expected kustomize1 and kustomize2 to be in \$PATH"
+    >&2 echo "failed to find all dependencies."
+    >&2 echo "Please set KUSTOMIZE1 and KUSTOMIZE3, or ensure that the binaries kustomize and kustomize3 are in your \$PATH"
     exit 1
 fi
 
 res=0
+overall=PASS
 
 run_test "v1-only_with_k1" $k1 ./v1-only 0 || res=1
 run_test "v1-only_with_k3" $k3 ./v1-only 1 || res=1
@@ -57,4 +59,9 @@ run_test "v3-only_with_k3" $k3 ./v3-only 0 || res=1
 run_test "v1-and-v3_with_k1" $k1 ./v1-and-v3 0 || res=1
 run_test "v1-and-v3_with_k3" $k3 ./v1-and-v3 0 || res=1
 
+if [ $res -ne 0 ]; then
+    overall=FAIL
+fi
+
+echo "Overall result: $overall"
 exit $res
